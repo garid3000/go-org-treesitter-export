c4e8de762f42d3be4c621469c5de03cfa927ba16
garid3000 <garidzorigoo@gmail.com>
2024-05-06 19:29:17 +0900

    Update

--- main.go
+++ main.go
@@ -5,56 +5,92 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	sitter "github.com/garid3000/go-tree-sitter" //sitter "github.com/smacker/go-tree-sitter"
 	"github.com/garid3000/go-tree-sitter/org"
+
+	"hash/maphash"
 )
 
-var fileString = []byte(``)
-var outputFile *os.File
+var fileString []byte             // this contains the source code
+var outputFile *os.File           // this for the ooutput file
+var treeNodeId = make([]int, 100) // this contains the id
+var hmap maphash.Hash
 
-var treeNodeId = []int{}
+// var map_of_nodes map[string]string // 1-1-2 -> uint64
 
-//
-//  <h2 id="orgfbd0474">
-//  <span class="section-number-2">1.</span> lvl1 heading Lorem ipsum
-//  </h2>
-//
-//  <div class="outline-text-2" id="text-1">
-//  | <p>
-//  |   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat
-//  |   rhoncus ex at varius. Etiam lacinia ex nisl, ac vestibulum lacus
-//  |   interdum pellentesque. Pellentesque rhoncus quis sem vitae convallis.
-//  | </p>
-//  </div>
-
-func list_of_int_2_string_as_id(alist []int) string {
-	result := "text"
-	for _, element := range alist {
-		result += fmt.Sprintf("-%d", element)
+func slice_of_int_to_string(alist []int, depht int) string {
+	result := ""
+	for i, element := range alist {
+		if i >= depht {
+			if i != 0 {
+				result = result[:len(result)-1]
+				// just to make sure it won't end with -
+			}
+			break
+		}
+		result += fmt.Sprintf("%d-", element)
 	}
 	return result
 }
 
-func VisitNode(node *sitter.Node, depth int, outputfile *os.File) {
-	treeNodeId = append(treeNodeId, 0)
-	fmt.Println(treeNodeId, "\t", node.Type(), "\t|\t", "\t^\t", node.Content(fileString)) // Symbol ~~ type
+func slice_of_int_to_hash_string(alist []int, depht int) string {
+	hmap.SetSeed(hmap.Seed())
+	hmap.WriteString(slice_of_int_to_string(alist, depht)) // should have error handler?
+	return strconv.FormatUint(hmap.Sum64(), 10)
+}
 
-	h_int := len(treeNodeId) - 1
+func VisitNode(node *sitter.Node, depth int, outputfile *os.File) {
+	//treeNodeId = append(treeNodeId, 0)
+	fmt.Println(slice_of_int_to_string(treeNodeId, depth), "\t", depth, "-depth", node.Type(), "\t|\t", "\t^\t", node.Content(fileString)) // Symbol ~~ type
+
 	switch node.Type() {
 	case "headline":
-		fmt.Println("FUCKING HEALDINE")
 		outputfile.WriteString(
 			fmt.Sprintf(
-				"<h%d id=\"%s\"> <span class=\"section-number-2\"> %s </span> %s </h%d>\n",
-				h_int, "orgfbd0474", list_of_int_2_string_as_id(treeNodeId),  node.Content(fileString), h_int,
+				"<h%d id=\"%s\">",
+				depth-1, slice_of_int_to_hash_string(treeNodeId, depth-1),
+				// depth should section's which is parent of headline?
 			),
 		)
+	case "section":
+		outputfile.WriteString(
+			fmt.Sprintf(
+				"<div id=\"outline-container-%s\" class=\"outline-%d\">",
+				slice_of_int_to_hash_string(treeNodeId, depth), depth,
+			),
+		)
+
+	case "stars":
+		outputfile.WriteString(
+			fmt.Sprintf(
+				"<span class=\"section-number-%d\">%s ",
+				// depth is depth section which is grandparent of stars
+				// section -> headline -> stars
+				depth-2,
+				slice_of_int_to_string(treeNodeId, depth-2),
+			),
+		)
+
+	case "item":
+		outputfile.WriteString(
+			fmt.Sprintf("%s", node.Content(fileString)),
+		)
+
 	case "body":
 		outputfile.WriteString(
 			fmt.Sprintf(
-				"<div class=\"outline-text-%d\" id=\"%s\"> <p> %s </p> </div>\n",
-				h_int, list_of_int_2_string_as_id(treeNodeId),  node.Content(fileString),
+				"<div class=\"outline-text-%d\" id=\"text-%s\">",
+				depth, slice_of_int_to_string(treeNodeId, depth),
+			),
+		)
+
+	case "paragraph":
+		outputfile.WriteString(
+			fmt.Sprintf(
+				"<p>%s",
+				node.Content(fileString),
 			),
 		)
 
@@ -62,11 +98,42 @@ func VisitNode(node *sitter.Node, depth int, outputfile *os.File) {
 	}
 
 	for i := 0; i < int(node.ChildCount()); i++ {
-		treeNodeId[len(treeNodeId)-1] = i
+		//treeNodeId[len(treeNodeId)-1] = i
+		treeNodeId[depth] = i
+		treeNodeId[depth+1] = 0
 		child_node := node.Child(i)
 		VisitNode(child_node, depth+1, outputfile)
 	}
-	treeNodeId = treeNodeId[0 : len(treeNodeId)-1]
+	// treeNodeId = treeNodeId[0 : len(treeNodeId)-1]
+
+	switch node.Type() {
+	case "headline":
+		outputfile.WriteString(
+			fmt.Sprintf("</h%d>", depth),
+		)
+	case "section":
+		outputfile.WriteString(
+			fmt.Sprintf("</div>"),
+		)
+
+	case "item":
+		outputfile.WriteString("")
+	case "stars":
+		outputfile.WriteString(
+			fmt.Sprintf("</span>"),
+		)
+	case "body":
+		outputfile.WriteString(
+			fmt.Sprintf("</div>"),
+		)
+	case "paragraph":
+		outputfile.WriteString(
+			fmt.Sprintf("</p>"),
+		)
+
+	default:
+	}
+
 }
 
 func main() {
@@ -81,7 +148,7 @@ func main() {
 	lang := org.GetLanguage()
 
 	//open file to output
-	outputfile, err := os.OpenFile("/tmp/asdf.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+	outputfile, err := os.OpenFile("/tmp/asdf.html", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
 		log.Fatal(err)
 	}
